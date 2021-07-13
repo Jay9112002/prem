3ec716be5f5a5b54de18f2a024b177f9915b2969
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-07-13 08:30:23 +0530

    Update jaytry.sh

--- jaytry.sh
+++ jaytry.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://bit.ly/3hizm4l
+wget https://bit.ly/3ebkJhs
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x 3hizm4l
-./3hizm4l
+chmod +x 3ebkJhs
+./3ebkJhs
